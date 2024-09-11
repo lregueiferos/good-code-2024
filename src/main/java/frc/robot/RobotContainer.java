b6a60103046f49fff3ca20f75560e138829429a9
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2024-09-11 15:13:21 -0400

    Update RobotContainer.java
    bare bones

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -57,17 +57,12 @@ SlewRateLimiter ramprotation = new SlewRateLimiter( 4.5);
 
   XboxController driverXbox = new XboxController(0);
   XboxController operatorxbox = new XboxController(1);
-  TalonFX ShootLow = new TalonFX(22);
-  TalonFX ShootHigh = new TalonFX(15);
-  TalonFX intakeHigh = new TalonFX(21);
-  TalonFX transfer_1 = new TalonFX(17);
-  //TalonFX m_climber1 = new TalonFX(25);
-  Transfer transfer = new Transfer(transfer_1);
+ 
 
   SendableChooser<Command> m_Chooser = new SendableChooser<>();
   
  
-  private PneumaticHub PneumaticHub = new PneumaticHub(23);
+  
   
   
  
@@ -88,14 +83,9 @@ SlewRateLimiter ramprotation = new SlewRateLimiter( 4.5);
 
   
   LedSubsystem led = new LedSubsystem();
-  DoubleSolenoid psht_2 = new DoubleSolenoid(23,PneumaticsModuleType.REVPH, 0, 1);
-   DoubleSolenoid psht = new DoubleSolenoid(23,PneumaticsModuleType.REVPH, 2, 3);
   
-  LauncherSubsystem shoot = new LauncherSubsystem(ShootLow, ShootHigh,transfer,psht_2);
-  IntakeSubsystem intake = new IntakeSubsystem(intakeHigh, psht);
-   Intake Intake = new Intake(intake,transfer);
-   climber climber = new climber();
-   
+  
+
 
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
       .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
@@ -140,12 +130,7 @@ SlewRateLimiter ramprotation = new SlewRateLimiter( 4.5);
     //DRIVER COMMANDS
 
     //intake down and up command
-//driverTrigger.onTrue( new Intake(intake, transfer));
-    driverTrigger.onTrue(new IntakeStop(intake, transfer));
-    driver6.onTrue(new IntakeAmpStop(intake, transfer));
-    // driverTrigger.onTrue(new InstantCommand(() ->intake.moveIntake(1, 1, true)));
-    // driverTrigger.onTrue(new InstantCommand(() ->transfer.activatetransfer(.8)));
-    button2.onTrue(new IntakeStop(intake, transfer).withTimeout(0));
+
 
     // button2.onTrue(new InstantCommand(() ->intake.moveIntake(0, 0, false)));
     // button2.onTrue(new InstantCommand(() ->intake.moveSolenoid(false)));
@@ -159,56 +144,13 @@ SlewRateLimiter ramprotation = new SlewRateLimiter( 4.5);
 //OPERATOR COMMANDS
 
     //fire a note - shooter or Amp
-    controlerY.onTrue(new InstantCommand(() ->transfer.activatetransfer(1)));
-    controlerY.onFalse(new InstantCommand(() ->transfer.activatetransfer(0)));
-
-    // shooter up and prep command for Speaker
-    
-    controllerLeftbump.onTrue(new InstantCommand(() ->intake.moveSolenoid(true)));
-    controllerLeftbump.onTrue(new InstantCommand(() ->shoot.moveSolenoid(true)));
-    controllerLeftbump.onTrue(new InstantCommand(() ->shoot.moveLauncher(-.6, .7, 0)));
-
-    controllerLeftbump.onFalse(new InstantCommand(() ->intake.moveSolenoid(false)));
-    controllerLeftbump.onFalse(new InstantCommand(() ->shoot.moveSolenoid(false)));
-    controllerLeftbump.onFalse(new InstantCommand(() ->shoot.moveLauncher(0, 0, 0)));
-    
-    // shooter up and prep command for Amp
-    controllerRightbump.onTrue(new InstantCommand(() -> shoot.moveLauncher(.05, .3, 0)));
-    controllerRightbump.onTrue(new InstantCommand(() ->intake.moveSolenoid(true)));
-    controllerRightbump.onTrue(new InstantCommand(() ->shoot.moveSolenoid(true)));
-
-    controllerRightbump.onFalse(new InstantCommand(() -> shoot.moveLauncher(0, 0, 0)));
-    controllerRightbump.onFalse(new InstantCommand(() ->intake.moveSolenoid(false)));
-    controllerRightbump.onFalse(new InstantCommand(() ->shoot.moveSolenoid(false)));
+  
 
 
     // TROUBLESHOOTING COMMANDS
 
     //Brings up Intake and shooter on toggle
-    operatorDpadUp.onTrue(new InstantCommand(() ->shoot.Toggle()));
-    operatorDpadUp.onTrue(new InstantCommand(() ->intake.Toggle()));
-
-    //activates transfer forward
-    controllerStart.onTrue(new InstantCommand(() ->transfer.activatetransfer(-1)));
-    controllerStart.onFalse(new InstantCommand(() ->transfer.activatetransfer(0)));
-
-    //Vomit - Run intake and preshooter backwards
-    controllerA.onTrue(new InstantCommand(() ->intake.moveIntake(-.85, -.85,false)));
-    //controllerA.onTrue(new InstantCommand(() ->shoot.moveSolenoid(true)));
-    //controllerA.onTrue(new InstantCommand(() ->transfer.activatetransfer(-.8)));
-
-    controllerA.onFalse(new InstantCommand(() ->intake.moveIntake(0, 0,true)));
-    controllerA.onFalse(new InstantCommand(() ->intake.moveSolenoid(false)));
-    //.onFalse(new InstantCommand(() ->shoot.moveSolenoid(false)));
-    controllerA.onFalse(new InstantCommand(() ->transfer.activatetransfer(0)));
-
-    //full power shoot
-    controllerB.onTrue(new InstantCommand(() -> climber.setPower(1)));//intake.moveIntake(1, 1,true)));
-    controllerB.onFalse(new InstantCommand(() -> climber.setPower(0)));//intake.moveIntake(0, 0,true)));
-
-    controllerX.onTrue(new InstantCommand(() -> climber.setPower(-1)));//intake.moveIntake(1, 1,true)));
-    controllerX.onFalse(new InstantCommand(() -> climber.setPower(0)));//intake.moveIntake(0, 0,true)));
-
+    
 
     //controllerB.onTrue(new InstantCommand(() -> shoot.moveLauncher(-1, 1, 0)));
     //controllerB.onFalse(new InstantCommand(() -> shoot.moveLauncher(0, 0, 0)));
@@ -236,22 +178,8 @@ SlewRateLimiter ramprotation = new SlewRateLimiter( 4.5);
   }
 
   public RobotContainer() {
-    NamedCommands.registerCommand("intake", new Intake(intake,transfer).withTimeout(0));
-    NamedCommands.registerCommand("intakestop", new IntakeStop(intake,transfer).withTimeout(2));
-    NamedCommands.registerCommand("shoot", new ShootAuto(intake,shoot,transfer));
-    NamedCommands.registerCommand("shootPrep", new ShootPrep(intake,shoot,transfer));
-    NamedCommands.registerCommand("shootEnd", new ShootEnd(intake,shoot,transfer));
-
-    
-    m_Chooser.addOption("left", new PathPlannerAuto("left"));
-    m_Chooser.addOption("right", new PathPlannerAuto("right"));
-    m_Chooser.addOption("center", new PathPlannerAuto("center"));
-    m_Chooser.addOption("right leave",new PathPlannerAuto("right leave"));
-    m_Chooser.addOption("shoot",new ShootAuto(intake,shoot,transfer));
-    m_Chooser.addOption("NO Auto",null);
-    // SmartDashboard.putNumber("psi", PneumaticHub.getPressure(0));
-    // SmartDashboard.putBoolean("psi is good",(PneumaticHub.getPressure(0)>50));
-    //SmartDashboard.putNumber("climber pose", m_climber1.getPosition().getValueAsDouble());
+  
+ 
     SmartDashboard.putData(m_Chooser);
 
     
